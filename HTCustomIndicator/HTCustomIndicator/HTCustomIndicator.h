c4a5c919c6b53e5c24e1edeaae84799c8bc641f2
HungTran <hung.trangia@apps-cyclone.com>
2018-09-10 09:30:10 +0700

    First commit (Nothing in here)

---
+++ HTCustomIndicator/HTCustomIndicator/HTCustomIndicator.h
@@ -0,0 +1,19 @@
+//
+//  HTCustomIndicator.h
+//  HTCustomIndicator
+//
+//  Created by UltraHigh on 9/10/18.
+//  Copyright © 2018 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTCustomIndicator.
+FOUNDATION_EXPORT double HTCustomIndicatorVersionNumber;
+
+//! Project version string for HTCustomIndicator.
+FOUNDATION_EXPORT const unsigned char HTCustomIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTCustomIndicator/PublicHeader.h>
+
+
---
+++ HTCustomIndicator/HTCustomIndicator/HTIndicator.swift
@@ -0,0 +1,22 @@
+//
+//  CustomIndicator.swift
+//  HTCustomIndicator
+//
+//  Created by UltraHigh on 9/10/18.
+//  Copyright © 2018 HT. All rights reserved.
+//
+
+import Foundation
+
+public class HTIndicator {
+    
+    private init() {}
+    
+    public static func create() -> String {
+        return "Success"
+    }
+}
+
+
+
+
