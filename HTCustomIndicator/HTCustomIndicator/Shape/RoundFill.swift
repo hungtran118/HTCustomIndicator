b126e2732da7a8e1954996ad7989ed8c57f09617
HungTran <hung.trangia@apps-cyclone.com>
2018-09-10 14:43:11 +0700

    test create

---
+++ HTCustomIndicator/HTCustomIndicator/Shape/RoundFill.swift
@@ -0,0 +1,24 @@
+//
+//  RoundFill.swift
+//  HTCustomIndicator
+//
+//  Created by UltraHigh on 9/10/18.
+//  Copyright © 2018 HT. All rights reserved.
+//
+
+import UIKit
+
+class RoundFill: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = min(frame.width, frame.height) / 2
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+
--- HTCustomIndicatorExample/Pods/Target Support Files/HTCustomIndicator/HTCustomIndicator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HTCustomIndicator : NSObject
-@end
-@implementation PodsDummy_HTCustomIndicator
-@end
